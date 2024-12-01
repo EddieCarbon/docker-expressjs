0eac8431a2ced37192d7693fc836b1a91d030364
Kuba Tomiczek <92593243+EddieCarbon@users.noreply.github.com>
2024-12-01 16:00:32 +0100

    Feature: docker-compose(#3)
    * Docker configuration
    
    * Added mongodb integrated with docker-compose

---
+++ app/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+  });
+
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+app.post('/addUser', async (req, res) => {
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email
+    });
+
+    await user.save();
+    res.status(201).send('User added successfully');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to add user');
+  }
+});
+
+// Pobierz wszystkich użytkowników z bazy danych
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to fetch users');
+  }
+});
+
+// Główny endpoint
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
